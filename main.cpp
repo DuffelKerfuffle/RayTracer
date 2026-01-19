8e0885b3f7f29c286a9a95db2fd64e4c91a035f7
= <maulik.tripathi.23@ucl.ac.uk>
2026-01-19 18:37:28 +0000

    created vector library

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "vec3.h"
+#include "colour.h"
+
 #include <iostream>
 
 int main() {
@@ -8,16 +11,12 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for(int j = 0; j < image_height; j++){
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_height; i++){
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n'; 
+        
+            auto pixel_colour = colour(double(i)/(image_width-1), double(j) / (image_height-1), 0);
+            write_colour(std::cout, pixel_colour);
         }
     }
+    std::clog << "\rDone.                 \n";
 }
