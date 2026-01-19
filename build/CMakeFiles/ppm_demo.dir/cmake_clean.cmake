f75310b0a1c91d4f37abc55cf6dfede4a373cc8f
= <maulik.tripathi.23@ucl.ac.uk>
2026-01-19 17:57:23 +0000

    created basic image

---
+++ build/CMakeFiles/ppm_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ppm_demo.dir/main.cpp.o"
+  "CMakeFiles/ppm_demo.dir/main.cpp.o.d"
+  "ppm_demo"
+  "ppm_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ppm_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
