63a47ff537cf3a64fe4b221143258ae757e6ad04
= <maulik.tripathi.23@ucl.ac.uk>
2026-01-27 22:10:24 +0000

    made code more modular and implemented lambertian diffusion

--- colour.h
+++ colour.h
@@ -2,17 +2,33 @@
 #define COLOUR_H
 
 #include "vec3.h"
+#include "interval.h"
 
 using colour = vec3;
+
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
 
 void write_colour(std::ostream&out, const colour& pixel_colour){
     auto r = pixel_colour.x();
     auto g = pixel_colour.y();
     auto b = pixel_colour.z();
 
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
